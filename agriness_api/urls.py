062d8dfd5591e50000e5f77998e6a72f3fa7d417
Mateus Veloso <mateusvsv@gmail.com>
2019-03-23 22:43:26 -0300

    Adiciona Swagger para documentação da API.

--- agriness_api/urls.py
+++ agriness_api/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from rest_framework_swagger.views import get_swagger_view
 
 
-schema_view = get_swagger_view(title='API')
+schema_view = get_swagger_view(title='Agriness CMS API - Endpoints')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
